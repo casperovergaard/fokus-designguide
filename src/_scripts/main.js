e08ac5de69766bf78c431db074747c00792ce10c
Casper Overgaard <account@casperovergaard.dk>
2016-12-20 18:26:12 +0100

    tweaks and new stuff

--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -8,13 +8,20 @@ import $ from 'jquery';
 import jQuery from 'jquery';
 import foundationPlugins from './components/foundationPlugins';
 import velocity from 'velocity-animate';
+import validation from 'jquery-validation';
 
 // App
 import smoothscroll from './components/smoothscroll'
 
 // Init
 $(() => {
+
+  $("#validate").focus();
+
+  $("#validate").val('Johnson.dk');
+
   $('.menu').velocity({ translateX: 0 });
-  new smoothscroll();
+  // new smoothscroll();
+  new validation();
   $(document).foundation();
 });
